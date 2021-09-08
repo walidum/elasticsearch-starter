c22ee9ca45a11d3d478c90b882f485cb7ceb4391
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-09-08 12:49:00 +0100

    second commit

---
+++ src/main/java/com/meylium/elsch/model/User.java
@@ -0,0 +1,27 @@
+package com.meylium.elsch.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(indexName = "users")
+public class User {
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text, name = "name")
+    private String name;
+
+    @Field(type = FieldType.Text, name = "username")
+    private String username;
+
+    @Field(type = FieldType.Text, name = "email")
+    private String email;
+}
