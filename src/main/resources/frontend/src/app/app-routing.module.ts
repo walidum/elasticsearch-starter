518b71d71f05ecfe37be0589383e7138d5f8e3f6
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-09-08 16:40:46 +0100

    first commit front end

--- src/main/resources/frontend/src/app/app-routing.module.ts
+++ src/main/resources/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./modules/home/home.component";
+import {UsersComponent} from "./modules/users/users.component";
+import {JobsComponent} from "./modules/jobs/jobs.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {path: 'home', component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'jobs', component: JobsComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/main/resources/frontend/src/app/app.module.ts
+++ src/main/resources/frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './modules/users/users.component';
+import { JobsComponent } from './modules/jobs/jobs.component';
+import { HomeComponent } from './modules/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    JobsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
