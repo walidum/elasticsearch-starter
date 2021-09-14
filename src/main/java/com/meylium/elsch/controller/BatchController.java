0f53aedc7255e78bd5ebf57f2ca56505397b485c
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-09-14 11:10:18 +0100

    Impliment the starting

--- src/main/java/com/meylium/elsch/controller/BatchController.java
+++ src/main/java/com/meylium/elsch/controller/BatchController.java
@@ -6,8 +6,6 @@ import com.meylium.elsch.util.JobExecutionDto;
 import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.NoSuchJobException;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,35 +18,32 @@ import java.util.stream.Collectors;
 @RequestMapping("batch")
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class BatchController {
+    
     private final BatchService batchService;
-    @Autowired
-    private JobRepository jobRepository;
-    @Autowired
-    private JobExplorer jobExplorer;
+    private final JobExplorer jobExplorer;
 
-    public BatchController(BatchService batchService) {
+    public BatchController(BatchService batchService, JobExplorer jobExplorer) {
         this.batchService = batchService;
+        this.jobExplorer = jobExplorer;
     }
 
-    @GetMapping(path = "importusers")
-    public ResponseEntity importUsers() {
+    @PutMapping(path = "start/{name}")
+    public ResponseEntity start(@PathVariable("name") final String name) {
         try {
-            batchService.startJobImportUsersFromApi();
+            switch (name) {
+                case "importFromRestApi":
+                    batchService.startJobImportUsersFromApi();
+                    break;
+                case "importFromCsv":
+                    batchService.startJobImportCarsFromCsv();
+                    break;
+            }
             return ResponseEntity.ok("Job started ...");
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
     }
 
-    @GetMapping(path = "importCars")
-    public ResponseEntity importCars() {
-        try {
-            batchService.startJobImportCarsFromCsv();
-            return ResponseEntity.ok("Job started ...");
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
-    }
 
     @GetMapping(path = "history/{name}")
     public ResponseEntity batchHistory(@PathVariable final String name) {
